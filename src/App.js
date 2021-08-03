d6bbdc66d77287b820f82e9a130a3aa6012c6b3a
Sungmin Oh <saogig0503@naver.com>
2021-08-03 22:52:28 +0900

    5강 deep copy로 state 변경하기

--- src/App.js
+++ src/App.js
@@ -4,9 +4,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let [글제목, 글제목변경] = useState(['남자 코트 추천', '스피커 추천', '쿠팡 쇼핑 꿀팁']);
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '쿠팡 쇼핑 꿀팁', '스피커 추천']);
   let [따봉, 따봉변경] = useState(0);
   let posts = '강남 고기 맛집';
+
+  function 제목바꾸기() {
+    var newArray = [...글제목];
+    newArray[0] = '여자 코트 추천';
+    글제목변경(newArray);
+  }
+
+  function 글정렬() {
+    var newArray = [...글제목];
+    newArray.sort();
+    글제목변경(newArray);
+  }
 
   return (
     <div className="App">
@@ -14,7 +26,7 @@ function App() {
         <div>개발 Blog</div>
       </div>
       <div className="list">
-        <h3> {글제목[0]} <span onClick={ () => {따봉변경(따봉 + 1)}}>👍</span> {따봉} </h3>
+        <h3> {글제목[0]} <span onClick={ 제목바꾸기 }>👍</span> {따봉} </h3>  
         <p>7월 27일 발행</p>
         <hr/>
       </div>
@@ -28,7 +40,8 @@ function App() {
         <p>8월 3일 발행</p>
         <hr/>
       </div>
-      <button onClick={ () => {글제목변경(['여자 코트 추천', '스피커 추천', '쿠팡 쇼핑 꿀팁'])}}>글 제목 변경</button>
+      <button onClick={ 제목바꾸기 }>글 제목 변경</button>
+      <button onClick={ 글정렬 }>글 정렬</button>
     </div>
   );
 }
